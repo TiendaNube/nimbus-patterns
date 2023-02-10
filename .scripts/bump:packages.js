a1c6b545ff0b8a95f9e935b6e49dddfceb5e4129
Júnior Conquista <conquista.engcomp@gmail.com>
2023-02-10 09:37:11 -0300

    feat: created script generate bump versions dependabot

---
+++ .scripts/bump:packages.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const { execSync } = require("child_process");
+
+const run = () => {
+  execSync("git diff --name-only master > ./.scripts/diff.txt");
+  const diff = glob.sync(path.join("./.scripts/diff.txt"));
+  if (!diff.length) {
+    throw new Error(
+      "The versions file was not found so it was not possible to perform the build..."
+    );
+  }
+  const content = fs.readFileSync(diff[0], "utf8");
+  console.log(`\x1b[32m 🏃‍♂️ Generating versions for packages... \x1b[0m`);
+
+  content
+    .match(/packages\/react\/\w+?-?\w+\/package\.json/gm)
+    .reduce((prev, curr) => {
+      const packageDir = curr.replace("/package.json", "");
+      execSync("yarn version -d minor", { cwd: packageDir });
+      return prev;
+    }, "");
+  execSync("rm -rf ./.scripts/diff.txt");
+};
+
+try {
+  run();
+} catch (err) {
+  console.error(`\x1b[33m ${err.message} \x1b[0m`);
+  process.exit(1);
+}
