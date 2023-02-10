c0b8a34e2b60d39effe8b8d8c1cf67bab4ffe303
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2023-02-10 14:13:17 -0300

    fix: icon color issue on menu footer

--- packages/react/Menu/src/components/MenuFooter/MenuFooter.tsx
+++ packages/react/Menu/src/components/MenuFooter/MenuFooter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { ChevronRightIcon } from "@tiendanube/icons";
 import { Box } from "@nimbus-ds/box";
+import { Icon } from "@nimbus-ds/icon";
 import { MenuButton } from "@nimbus-ds/menubutton";
 
 import { MenuFooterProps } from "./menuFooter.types";
@@ -10,20 +11,25 @@ const MenuFooter: React.FC<MenuFooterProps> = ({
   className: _className,
   style: _style,
   ...rest
-}: MenuFooterProps) => (
-  <Box
-    boxSizing="border-box"
-    display="flex"
-    flex="0 1 auto"
-    paddingX="2"
-    paddingY="4"
-    width="100%"
-  >
-    <MenuButton {...rest}>
-      <ChevronRightIcon />
-    </MenuButton>
-  </Box>
-);
+}: MenuFooterProps) => {
+  const activeColor = rest.active ? "primary.interactive" : "primary.textHigh";
+  const disabledColor = rest.disabled ? "neutral.textDisabled" : activeColor;
+
+  return (
+    <Box
+      boxSizing="border-box"
+      display="flex"
+      flex="0 1 auto"
+      paddingX="2"
+      paddingY="4"
+      width="100%"
+    >
+      <MenuButton {...rest}>
+        <Icon source={<ChevronRightIcon />} color={disabledColor} />
+      </MenuButton>
+    </Box>
+  );
+};
 
 MenuFooter.displayName = "Menu.Footer";
 
