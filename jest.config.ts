8ddaf7a5ae16cfb3d7546a71fb84da94b4860083
Júnior Conquista <conquista.engcomp@gmail.com>
2023-02-13 12:49:47 -0300

    fix: fixename named file

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,12 @@ const config: Config.InitialOptions = {
     ".+\\.(ts|tsx)$": "ts-jest",
   },
   coveragePathIgnorePatterns: ["dist"],
+  moduleNameMapper: {
+    // atomics
+    "@nimbus-ds/menubutton": "<rootDir>/packages/react/MenuButton/src",
+    "@nimbus-ds/menu": "<rootDir>/packages/react/Menu/src",
+    "@nimbus-ds/page": "<rootDir>/packages/react/Page/src",
+  },
 };
 
 export default config;
