57c1ec515a9831661fbd230c352b6e130e09a42a
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2023-03-08 11:11:07 -0300

    chore: prepare versions

--- packages/react/DataList/src/components/DataListRow/dataListRow.types.ts
+++ packages/react/DataList/src/components/DataListRow/dataListRow.types.ts
@@ -2,7 +2,8 @@ import { ReactNode, HTMLAttributes } from "react";
 
 import { BoxProps } from "@nimbus-ds/components";
 
-type Extend = Omit<HTMLAttributes<HTMLElement>, "color"> & BoxProps;
+type Extend = Omit<HTMLAttributes<HTMLElement>, "color"> &
+  Omit<BoxProps, "padding">;
 
 export interface DataListRowProps extends Extend {
   padding?: "small" | "base" | "none";
