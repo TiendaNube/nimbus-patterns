fb1a46e8d0c6f67fe04709790bf54279e8c2ec4c
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2023-03-16 13:38:30 -0300

    feat: create new empty message component

---
+++ packages/react/EmptyMessage/src/index.ts
@@ -0,0 +1,5 @@
+import { EmptyMessage } from "./EmptyMessage";
+
+export { EmptyMessage } from "./EmptyMessage";
+export type { EmptyMessageProps } from "./emptyMessage.types";
+export default EmptyMessage;
