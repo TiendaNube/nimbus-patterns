eb9b68f5345d6c95149e16fb9db503d9d8f49d46
Júnior Conquista <conquista.engcomp@gmail.com>
2023-04-06 10:49:50 -0300

    refactor: restructured packages in the project

---
+++ packages/react/src/index.ts
@@ -0,0 +1,15 @@
+// components
+export * from "@nimbus-ds/app-shell";
+export * from "@nimbus-ds/callout-card";
+export * from "@nimbus-ds/data-list";
+export * from "@nimbus-ds/data-table";
+export * from "@nimbus-ds/empty-message";
+export * from "@nimbus-ds/interactive-list";
+export * from "@nimbus-ds/layout";
+export * from "@nimbus-ds/menu";
+export * from "@nimbus-ds/menubutton";
+export * from "@nimbus-ds/nav-tabs";
+export * from "@nimbus-ds/page";
+export * from "@nimbus-ds/productupdates";
+export * from "@nimbus-ds/sidemodal";
+export * from "@nimbus-ds/thumbnail-with-action";
