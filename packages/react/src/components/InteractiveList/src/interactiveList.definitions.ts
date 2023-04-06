f31250cc3c04240b427ed10ee9ef63aaa0af30ce
Júnior Conquista <conquista.engcomp@gmail.com>
2023-04-06 12:05:48 -0300

    chore: updated libraries

--- packages/react/src/components/InteractiveList/src/interactiveList.definitions.ts
+++ packages/react/src/components/InteractiveList/src/interactiveList.definitions.ts
@@ -1,4 +1,2 @@
-const uniqueId = () => `${Math.floor(Math.random() * Date.now() + 1)}`;
-
 export const generateID = (name: string) =>
-  `${name.toLowerCase().replace(/ /g, "-")}_${uniqueId()}`;
+  `${name.toLowerCase().replace(/ /g, "-")}_${new Crypto().randomUUID()}`;
