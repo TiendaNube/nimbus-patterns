32c61eccba1859655c5e3e7e21ec41e656d43ffd
Júnior Conquista <conquista.engcomp@gmail.com>
2023-04-11 09:14:19 -0300

    chore: publish release candidate

--- packages/react/src/components/InteractiveList/src/components/InteractiveListButtonItem/interactiveListButtonItem.types.ts
+++ packages/react/src/components/InteractiveList/src/components/InteractiveListButtonItem/interactiveListButtonItem.types.ts
@@ -2,15 +2,17 @@ import { HTMLAttributes } from "react";
 import { IconButtonProperties } from "@nimbus-ds/components";
 import { InteractiveListStructureProperties } from "../InteractiveListStructure/interactiveListStructure.types";
 
+type IconButtonProps = Omit<
+  IconButtonProperties,
+  "backgroundColor" | "borderColor" | "size" | "source"
+> & { onClick: () => void };
+
 export interface InteractiveListButtonItemProperties
   extends InteractiveListStructureProperties {
   /**
    * Button props inherited from Nimbus components
    */
-  iconButton: Omit<
-    IconButtonProperties,
-    "backgroundColor" | "borderColor" | "size" | "source"
-  >;
+  iconButton: IconButtonProps;
 }
 
 export type InteractiveListButtonItemProps =
