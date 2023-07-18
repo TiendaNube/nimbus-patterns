d925cc71e3184a9f3d95a2bc749008ecc276c213
Júnior Conquista <conquista.engcomp@gmail.com>
2023-07-18 10:45:22 -0300

    feat: added ref property to FormField.Select component API

--- packages/react/src/components/FormField/src/components/FormFieldSelect/formFieldSelect.types.ts
+++ packages/react/src/components/FormField/src/components/FormFieldSelect/formFieldSelect.types.ts
@@ -1,9 +1,10 @@
 import { HTMLAttributes } from "react";
-import { SelectProperties } from "@nimbus-ds/components";
+import { SelectProperties, SelectProps } from "@nimbus-ds/components";
 import { FormFieldProperties } from "../../formField.types";
 
 export type FormFieldSelectProperties = Omit<FormFieldProperties, "children"> &
   Omit<SelectProperties, "appearance">;
 
 export type FormFieldSelectProps = FormFieldSelectProperties &
+  Omit<SelectProps, "appearance"> &
   HTMLAttributes<HTMLElement>;
