e33a0b4f8a924ef4535591b5d8fc33ca67283375
Júnior Conquista <conquista.engcomp@gmail.com>
2023-10-18 00:41:17 -0300

    feat: added new ClearFormatting plugin to the editor

--- packages/react/src/components/Editor/src/actions/ClearFormatting/ClearFormatting.tsx
+++ packages/react/src/components/Editor/src/actions/ClearFormatting/ClearFormatting.tsx
@@ -3,19 +3,21 @@ import { Icon } from "@nimbus-ds/components";
 import { RemoveFormatIcon } from "@nimbus-ds/icons";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
-import { TollbarButton } from "../../components";
-import { CLEAR_FORMATING_COMMAND } from "../../utils";
+import { Button } from "../../components";
+import { CLEAR_FORMATTING_COMMAND } from "../../utils";
 
 const ClearFormatting: React.FC = () => {
   const [editor] = useLexicalComposerContext();
 
   return (
-    <TollbarButton
+    <Button
       aria-label="Undo"
-      onClick={() => editor.dispatchCommand(CLEAR_FORMATING_COMMAND, undefined)}
+      onClick={() =>
+        editor.dispatchCommand(CLEAR_FORMATTING_COMMAND, undefined)
+      }
     >
       <Icon source={<RemoveFormatIcon />} color="currentColor" />
-    </TollbarButton>
+    </Button>
   );
 };
 
