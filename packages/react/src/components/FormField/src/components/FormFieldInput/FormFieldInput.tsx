ddd4dea5ce548313a1efcbdcfd77b310f1b6d972
Rocío Esteban <53129859+hrchioest@users.noreply.github.com>
2023-12-05 00:57:15 -0300

    fix: add forwardRef to FormFieldInput

--- packages/react/src/components/FormField/src/components/FormFieldInput/FormFieldInput.tsx
+++ packages/react/src/components/FormField/src/components/FormFieldInput/FormFieldInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { Input } from "@nimbus-ds/components";
 
@@ -6,24 +6,29 @@ import { FormField } from "../../FormField";
 import { FormFieldInputProps } from "./formFieldInput.types";
 import { inputAppearance } from "../../formField.definitions";
 
-const FormFieldInput: React.FC<FormFieldInputProps> = ({
-  label,
-  helpText,
-  helpIcon: IconSrc,
-  appearance = "none",
-  showHelpText = false,
-  ...rest
-}) => (
-  <FormField
-    label={label}
-    helpText={helpText}
-    helpIcon={IconSrc}
-    appearance={appearance}
-    showHelpText={showHelpText}
-    id={rest.id}
-  >
-    <Input {...rest} appearance={inputAppearance[appearance]} />
-  </FormField>
+const FormFieldInput = forwardRef<HTMLInputElement, FormFieldInputProps>(
+  (
+    {
+      label,
+      helpText,
+      helpIcon: IconSrc,
+      appearance = "none",
+      showHelpText = false,
+      ...rest
+    },
+    ref
+  ) => (
+    <FormField
+      label={label}
+      helpText={helpText}
+      helpIcon={IconSrc}
+      appearance={appearance}
+      showHelpText={showHelpText}
+      id={rest.id}
+    >
+      <Input ref={ref} {...rest} appearance={inputAppearance[appearance]} />
+    </FormField>
+  )
 );
 
 export { FormFieldInput };
