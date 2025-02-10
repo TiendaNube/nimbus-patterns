4a8f186c35ade9c71fc1ded33253ce75f138252b
Joaquin Tornello <jtornello2019@hotmail.com>
2025-02-10 14:42:01 -0300

    feat: added docs

--- packages/react/src/components/PlanDisplay/src/planDisplay.types.ts
+++ packages/react/src/components/PlanDisplay/src/planDisplay.types.ts
@@ -10,12 +10,40 @@ import {
   PlanDisplayCardSpacing,
 } from "./components";
 
+/**
+ * Interface representing the internal components used in the PlanDisplay.
+ */
 export interface PlanDisplayComponents {
+  /**
+   * The card component used to display the plan. This component is used to wrap the header, content, and footer components, and it is able
+   * to receive a 'highlighted' prop to feature a plan.
+   */
   Card: typeof PlanDisplayCard;
+
+  /**
+   * The header component of the plan display card. This component is used to display the title and subtitle of the plan, along with
+   * an optional children where additional information can be displayed.
+   */
   Header: typeof PlanDisplayCardHeader;
+
+  /**
+   * The content component of the plan display card. This component is used to display the main content of the plan, such as the plan bullet points.
+   */
   Content: typeof PlanDisplayCardContent;
+
+  /**
+   * The spacing component used within the plan display card. This component is used to add spacing between the header, content, and footer components, displaying a line divider between them.
+   */
   Spacing: typeof PlanDisplayCardSpacing;
+
+  /**
+   * The bullet component used to display the plan bullet points. It displays an icon and a text, and can receive a 'disabled' prop to show the bullet point as disabled.
+   */
   Bullet: typeof PlanDisplayBullet;
+
+  /**
+   * The footer component of the plan display card. This component is used to display the footer of the plan, such as the CTA button.
+   */
   Footer: typeof PlanDisplayCardFooter;
 }
 
