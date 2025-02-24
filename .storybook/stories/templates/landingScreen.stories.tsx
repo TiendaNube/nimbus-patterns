675b67723def91ea11ec84e85e7365b044f74452
Joaquín Tornello <jtornello2019@hotmail.com>
2025-02-24 13:14:37 -0300

    feat: create Landing Screen template (#102)
    * feat: initial development of LandingScreen
    
    * feat: responsive design and added card
    
    * feat: added .yml versioning file
    
    * feat: added README and tsconfig, webpack
    
    * fix: fixed typo and renamed bullet to LandingScreenBullet
    
    * fix: changed version to 0
    
    * feat: added .yml file
    
    * feat: added landing screen feature support
    
    * feat: extracted types for every component
    
    * fix: fixed import
    
    * feat: added module component for app modules
    
    * feat: added test
    
    * feat: added docs
    
    * feat: added storybook docs
    
    * feat: implemented controls for each of the components
    
    * fix: fixed margins
    
    * fix: restored callout card and improved docs
    
    * feat: added CHANGELOG file
    
    * feat: initial development of plan display
    
    * feat: renamed LandingScreen to InitialScreen as discussed
    
    * fix: fixed color
    
    * fix: correctly renamed file
    
    * feat: improved layout and added data
    
    * fix: added higlighted props
    
    * feat: fixed and finished layout template
    
    * feat: added stories
    
    * feat: unit tests
    
    * feat: ensured project builds correctly and added .yml file
    
    * feat: added template
    
    * feat: uploaded correct nuvem chage image and aligned content
    
    * fix: fixed versions and updated changelog
    
    * fix: removed imgs and improved image visualization

---
+++ .storybook/stories/templates/landingScreen.stories.tsx
@@ -0,0 +1,357 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Box, Button, Text, Thumbnail, Title } from "@nimbus-ds/components";
+import {
+  BagIcon,
+  BoxPackedIcon,
+  CheckIcon,
+  CloseIcon,
+  FireIcon,
+  TagIcon,
+  TiendanubeIcon,
+} from "@nimbus-ds/icons";
+
+import { PlanDisplay } from "@nimbus-ds/plan-display";
+import { InitialScreen } from "@nimbus-ds/initial-screen";
+
+const meta: Meta = {
+  title: "Templates/LandingScreen",
+  argTypes: {
+    children: { control: { disable: true } },
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj;
+
+/**
+ * The LandingScreen template is used to introduce users to an application or service, offering options to access more information or proceed further.
+ * It is composed of several sections, each with a different purpose, such as presenting the main features, benefits, and plans. It is a flexible template that can be customized to fit the needs of the application or service, combined with two main pattern components: InitialScreen and PlanDisplay.
+ */
+export const base: Story = {
+  render: () => (
+    <InitialScreen mx="auto">
+      <InitialScreen.Hero
+        subtitle={"META".toUpperCase()}
+        title="¡Vendé más con Instagram y Facebook!"
+        bullets={[
+          <InitialScreen.Bullet
+            icon={<FireIcon />}
+            text="Integración gratis y rápida"
+          />,
+          <InitialScreen.Bullet
+            icon={<BagIcon />}
+            text="Sincronización de catálogo en tiempo real"
+          />,
+          <InitialScreen.Bullet
+            icon={<TagIcon />}
+            text="Creación y seguimiento de campañas"
+          />,
+          <InitialScreen.Bullet
+            icon={<TiendanubeIcon />}
+            text="Medición de conversiones con API y píxel de Meta"
+          />,
+        ]}
+        actions={<Button appearance="primary">Conectar cuenta</Button>}
+        image={
+          <img
+            src="./static/product.jpg"
+            alt="hero image"
+            width="100%"
+            height="auto"
+          />
+        }
+      />
+      <InitialScreen.Section title="Beneficios">
+        <InitialScreen.CardLayout>
+          <InitialScreen.Card
+            icon={<BoxPackedIcon size="large" />}
+            title="Gestión unificada"
+            description="Ahorrá tiempo gestionando tus envíos de forma unificada. Vas a
+              poder generar, imprimir y pagar tus etiquetas, desde el
+              administrador de tu tienda."
+          />
+
+          <InitialScreen.Card
+            icon={<BoxPackedIcon size="large" />}
+            title="Gestión unificada"
+            description="Ahorrá tiempo gestionando tus envíos de forma unificada. Vas a
+              poder generar, imprimir y pagar tus etiquetas, desde el
+              administrador de tu tienda."
+          />
+          <InitialScreen.Card
+            icon={<BoxPackedIcon size="large" />}
+            title="Gestión unificada"
+            description="Ahorrá tiempo gestionando tus envíos de forma unificada. Vas a
+              poder generar, imprimir y pagar tus etiquetas, desde el
+              administrador de tu tienda."
+          />
+        </InitialScreen.CardLayout>
+      </InitialScreen.Section>
+      <InitialScreen.Section title="Detalle">
+        <InitialScreen.Feature
+          content={
+            <>
+              <InitialScreen.FeatureItem
+                title="1. Cargá saldo"
+                description="Podés hacerlo con tarjeta de crédito, débito o mercado pago."
+              >
+                <InitialScreen.FeatureItemSpacing />
+              </InitialScreen.FeatureItem>
+
+              <InitialScreen.FeatureItem
+                title="2. Generá la etiqueta"
+                description="Revisá los datos y en simples pasos creá la etiqueta."
+              >
+                <InitialScreen.FeatureItemSpacing />
+              </InitialScreen.FeatureItem>
+              <InitialScreen.FeatureItem
+                title="3. Imprimí la etiqueta"
+                description="Una vez impresa colocala en el paquete a enviar."
+              >
+                <InitialScreen.FeatureItemSpacing />
+              </InitialScreen.FeatureItem>
+              <InitialScreen.FeatureItem
+                title="4. Despachá el paquete"
+                description="Podés despachar el paquete en cualquier sucursal de Correo Argentino o Andreani, o puntos de despacho."
+              />
+            </>
+          }
+          image={
+            <Box
+              maxWidth="500px"
+              borderRadius="5"
+              borderColor="primary-interactive"
+              borderWidth="4"
+              borderStyle="solid"
+            >
+              <img
+                src="./static/nuvem-chat.png"
+                alt="NuvemChatView"
+                width="100%"
+              />
+            </Box>
+          }
+        />
+      </InitialScreen.Section>
+      <InitialScreen.Section title="Planes">
+        <PlanDisplay mx="4">
+          <PlanDisplay.Card>
+            <PlanDisplay.Header
+              subtitle="Punto de venta"
+              title={
+                <Box display="flex" gap="1">
+                  <Title as="h3" color="neutral-textLow">
+                    Plan
+                  </Title>
+                  <Title as="h3" color="neutral-textHigh">
+                    Básico
+                  </Title>
+                </Box>
+              }
+            >
+              <Title as="h5">Suscripción gratuita</Title>
+              <Text fontWeight="bold" fontSize="highlight">
+                1.5% costo por transacción
+              </Text>
+            </PlanDisplay.Header>
+            <PlanDisplay.Content>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Descuentos personalizados
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Registro de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Búsqueda de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Venta con envío
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Venta sin stock
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Registro de medio de pago
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Escaneo de producto
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Gestión de caja
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Asignación de vendedor
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Creación de productos durante la venta
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Guardar carritos
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Atajos de teclado
+              </PlanDisplay.Bullet>
+            </PlanDisplay.Content>
+          </PlanDisplay.Card>
+          <PlanDisplay.Card>
+            <PlanDisplay.Header
+              subtitle="Punto de venta"
+              title={
+                <Box display="flex" gap="1">
+                  <Title as="h3" color="neutral-textLow">
+                    Plan
+                  </Title>
+                  <Title as="h3" color="neutral-textHigh">
+                    Emprendedor
+                  </Title>
+                </Box>
+              }
+            >
+              <Title as="h5">$6.999 / mes</Title>
+              <Text fontWeight="bold" fontSize="highlight">
+                0.5% costo por transacción
+              </Text>
+            </PlanDisplay.Header>
+            <PlanDisplay.Content>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Descuentos personalizados
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Registro de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Búsqueda de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Venta con envío
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Venta sin stock
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Registro de medio de pago
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Escaneo de producto
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Gestión de caja
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Asignación de vendedor
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Creación de productos durante la venta
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Guardar carritos
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CloseIcon />} disabled>
+                Atajos de teclado
+              </PlanDisplay.Bullet>
+            </PlanDisplay.Content>
+          </PlanDisplay.Card>
+          <PlanDisplay.Card highlighted>
+            <PlanDisplay.Header
+              subtitle="Punto de venta"
+              title={
+                <Box display="flex" gap="1">
+                  <Title as="h3" color="neutral-textLow">
+                    Plan
+                  </Title>
+                  <Title as="h3" color="neutral-textHigh">
+                    Avanzado
+                  </Title>
+                </Box>
+              }
+            >
+              <Title as="h5">$14.999 / mes</Title>
+              <Text fontWeight="bold" fontSize="highlight">
+                0.15% costo por transacción
+              </Text>
+            </PlanDisplay.Header>
+            <PlanDisplay.Content>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Descuentos personalizados
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Registro de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet
+                icon={<CheckIcon fontWeight="bold" fontSize="24px" />}
+              >
+                Búsqueda de clientes
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Venta con envío
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Venta sin stock
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Registro de medio de pago
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Escaneo de producto
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Gestión de caja
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Asignación de vendedor
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Creación de productos durante la venta
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Guardar carritos
+              </PlanDisplay.Bullet>
+              <PlanDisplay.Bullet icon={<CheckIcon />}>
+                Atajos de teclado
+              </PlanDisplay.Bullet>
+            </PlanDisplay.Content>
+            <PlanDisplay.Footer>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                width="100%"
+                gap="2"
+              >
+                <Button appearance="primary">
+                  <Text fontSize="base" color="currentColor">
+                    Comenzar gratis por 7 días*
+                  </Text>
+                </Button>
+                <Text fontSize="caption">
+                  *Al finalizar la prueba podrás elegir cancelar, continuar o
+                  cambiar de plan
+                </Text>
+              </Box>
+            </PlanDisplay.Footer>
+          </PlanDisplay.Card>
+        </PlanDisplay>
+      </InitialScreen.Section>
+    </InitialScreen>
+  ),
+  args: {},
+};
