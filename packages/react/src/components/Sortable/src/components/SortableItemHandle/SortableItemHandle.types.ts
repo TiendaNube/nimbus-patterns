31dbc258509c0b6214ff5aecf891d22f2109c732
Joaquin Tornello <jtornello2019@hotmail.com>
2025-04-23 10:17:51 -0300

    fix: type

--- packages/react/src/components/Sortable/src/components/SortableItemHandle/SortableItemHandle.types.ts
+++ packages/react/src/components/Sortable/src/components/SortableItemHandle/SortableItemHandle.types.ts
@@ -1,4 +1,4 @@
 import { PropsWithChildren } from "react";
 
 // Properties specific to the SortableItemHandle component
-export type SortableItemHandleProperties = PropsWithChildren<{}>;
+export type SortableItemHandleProperties = PropsWithChildren;
