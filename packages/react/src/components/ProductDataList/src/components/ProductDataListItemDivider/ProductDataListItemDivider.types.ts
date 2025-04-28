d32c353fcb2adfb22ecdfc62179fb2d268b523be
Joaquin Tornello <jtornello2019@hotmail.com>
2025-04-28 10:06:59 -0300

    fix: types

--- packages/react/src/components/ProductDataList/src/components/ProductDataListItemDivider/ProductDataListItemDivider.types.ts
+++ packages/react/src/components/ProductDataList/src/components/ProductDataListItemDivider/ProductDataListItemDivider.types.ts
@@ -2,5 +2,5 @@ import { BoxProps } from "@nimbus-ds/components";
 
 export type ProductDataListItemDividerProperties = Pick<
   BoxProps,
-  "borderTopWidth" | "borderBottomWidth" | "borderColor" | "borderStyle"
+  "borderTopWidth" | "borderBottomWidth" | "borderColor" | "borderStyle" | "width" | "id"
 >;
