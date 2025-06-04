2f05a4d4114b900a6c27ce58d7e0ef10f34598d5
Joaquin Tornello <jtornello2019@hotmail.com>
2025-06-04 16:05:04 -0300

    feat: component bundle in webpack config file

--- packages/react/src/components/ProductUpdates/src/productUpdates.stories.tsx
+++ packages/react/src/components/ProductUpdates/src/productUpdates.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Thumbnail, Text } from "@nimbus-ds/components";
-
 import { ProductUpdates } from "./ProductUpdates";
 
 const meta: Meta<typeof ProductUpdates> = {
