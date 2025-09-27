9df8669e6c6bc3e5691c1d5b43ac8fe87187776c
Joaquin Tornello <jtornello2019@hotmail.com>
2025-09-27 20:04:53 -0300

    feat: improved docs

--- packages/react/src/components/AppShell/src/contexts/AppShellMenuContext/AppShellMenuContext.ts
+++ packages/react/src/components/AppShell/src/contexts/AppShellMenuContext/AppShellMenuContext.ts
@@ -6,6 +6,11 @@ export const AppShellMenuContext = createContext<
   AppShellMenuContextValue | undefined
 >(undefined);
 
+/**
+ * Read AppShellMenuContext state. Useful to know if the menu is rendered as a popover/flyout.
+ * @param enforce When false, returns a default value (false) if no provider is present. Adviced not to abuse, but useful for menu that Menu components that aren't always inside an AppShell.
+ * @throws If `enforce=true` and used without a provider.
+ */
 export const useAppShellMenuContext = (enforce = true) => {
   const context = useContext(AppShellMenuContext);
 
