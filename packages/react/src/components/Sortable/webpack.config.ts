e148b37ecb53b8272ae0cf13e9dbb49b7759f618
Joaquin Tornello <jtornello2019@hotmail.com>
2026-01-15 12:57:39 -0300

    fix: build by skipping lib check in sortable

--- packages/react/src/components/Sortable/webpack.config.ts
+++ packages/react/src/components/Sortable/webpack.config.ts
@@ -1,11 +1,19 @@
-import path from 'path';
-import { configuration } from '@nimbus-ds/patterns-webpack/src';
+import path from "path";
+import { configuration } from "@nimbus-ds/patterns-webpack/src";
 
 const config = {
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    library: '@nimbus-ds/sortable',
+    path: path.resolve(__dirname, "dist"),
+    library: "@nimbus-ds/sortable",
   },
 };
 
-export default () => configuration.getConfiguration(config); 
+// Skip lib check for Sortable. This is to avoid DND Kit type errors.
+const dtsBundleConfig = {
+  entries: [
+    "node ../../../../../node_modules/.bin/dts-bundle-generator -o ./dist/index.d.ts ./src/index.ts --external-imports @dnd-kit/core --external-imports @dnd-kit/sortable --external-imports @dnd-kit/utilities --no-check",
+  ],
+};
+
+export default () =>
+  configuration.getConfiguration(config, { dtsBundleConfig });
